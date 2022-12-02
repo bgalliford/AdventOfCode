a633568bd1775bb0cb99a8c35d52868982857bcb
Brian Galliford <galliford@geneseo.edu>
2022-12-02 10:52:05 -0500

    Appending with Switch

--- 2022/day02/day02_solve2.ps1
+++ 2022/day02/day02_solve2.ps1
@@ -5,7 +5,7 @@ $patterns = $guide | Sort-Object -Descending | Group-Object
 # Part 1 (incorrect guide interpretation)
 $scores = @()
 ForEach ($pattern in $patterns) {
-    $patternScore = Switch ($pattern.Name) {
+    $scores += Switch ($pattern.Name) {
         "A X" {4 * $pattern.Count}
         "A Y" {8 * $pattern.Count}
         "A Z" {3 * $pattern.Count}
@@ -16,14 +16,13 @@ ForEach ($pattern in $patterns) {
         "C Y" {2 * $pattern.Count}
         "C Z" {6 * $pattern.Count}
     }
-    $scores += $patternScore
 }
 $totalScore = ($scores | Measure-Object -Sum).Sum
 Write-Host "My total score, calculated incorrectly, is $totalScore"
 # Part 2 (correct guide interpretation)
 $scores = @()
 ForEach ($pattern in $patterns) {
-    $patternScore = Switch ($pattern.Name) {
+    $scores += Switch ($pattern.Name) {
         "A X" {3 * $pattern.Count}
         "A Y" {4 * $pattern.Count}
         "A Z" {8 * $pattern.Count}
@@ -34,7 +33,6 @@ ForEach ($pattern in $patterns) {
         "C Y" {6 * $pattern.Count}
         "C Z" {7 * $pattern.Count}
     }
-    $scores += $patternScore
 }
 $totalScore = ($scores | Measure-Object -Sum).Sum
 Write-Host "My total score, calculated correctly, is $totalScore"
